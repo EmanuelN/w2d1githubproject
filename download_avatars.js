9191730d4da49d2e7670af115f6c3dbcdbecaf2c
Emanuel Neves <emanuelneves10@gmail.com>
2017-07-31 17:29:08 +0000

    implemented downloadImageByURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 const GITHUB_USER = "EmanuelN";
 const GITHUB_TOKEN = "36412cc8405fa83d470e87a3c1324504524a98b4";
@@ -17,11 +18,25 @@ function getRepoContributors(repoOwner, repoName, cb){
   });
 }
 
-function callBack(err, res, body) {
+function getURLs(err, res, body) {
   if (err) throw err;
     var obj = JSON.parse(body)
+    var avatarURLS = []
     for (var i in obj){
-      console.log(obj[i].avatar_url);
+      avatarURLS.push(obj[i].avatar_url);
     }
 }
-getRepoContributors('jquery', 'jquery', callBack);
+
+function downloadImageByURL(url, filePath){
+  request.get(url)
+  .on('error', function(err){
+    throw err;
+  })
+  .on('response', function(response){
+    console.log("Response Status Code: ", response.statusCode);
+  })
+  .pipe(fs.createWriteStream(filePath));
+}
+getRepoContributors('jquery', 'jquery', getURLs);
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg")

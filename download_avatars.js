806e3588363ea928a35e35a437951ccc335f9d3a
Emanuel Neves <emanuelneves10@gmail.com>
2017-07-31 17:52:42 +0000

    added config file for secrets

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,10 @@
 var request = require('request');
 var fs = require('fs');
+require('dotenv').config()
 var ownerRepo = process.argv[2];
 var nameRepo = process.argv[3];
-
-const GITHUB_USER = "EmanuelN";
-const GITHUB_TOKEN = "36412cc8405fa83d470e87a3c1324504524a98b4";
+GITHUB_USER = process.env.GITHUB_USER;
+GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 function getRepoContributors(repoOwner, repoName, cb){
   var requestURL = "https://"+ GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + "/" + repoName + "/contributors";
